6da802e61a6b3b8864cdcb58d70c79fc079baf91
spiderforrest <spider@spood.org>
2023-03-05 14:58:12 -0800

    okay so that's args, coolio

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 import chalk from "chalk";
-import { ask } from "stdio";
+import { ask, getopt } from "stdio";
 
+const args = getopt({
+  tmp: { key: "n", args: 1, required: false },
+  _meta_: { minargs: 0 },
+});
 
-console.log(chalk.blue.bgRed.bold(await ask("i'll echo anything but in blue lol")));
+console.log(args);
+
+console.log(
+  chalk.blue.bgRed.bold(await ask("i'll echo anything but in blue lol"))
+);
