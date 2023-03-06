c49d1267eab3d73ccbf0c8c9f3cc2a87f53d5a17
spiderforrest <spider@spood.org>
2023-03-05 16:09:22 -0800

    primary arg layout

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 #!/usr/bin/env node
 import chalk from "chalk";
-import { ask, getopt } from "stdio";
+import { getopt } from "stdio";
 
-const args = getopt({
-  tmp: { key: "n", args: 1, required: false },
-  _meta_: { minargs: 0 },
+const opt = getopt({
+  _meta_: { minargs: 2 },
 });
+switch (opt.args[0]) {
+  case "a":
+  case "add":
+    console.log("add mode");
+    break;
+  case "x":
+  case "done":
+    console.log("done mode");
+    break;
+  case "r":
+  case "remove":
+  case "delete":
+    console.log("remove mode");
+    break;
+  case "m":
+  case "modify":
+    console.log("modify mode");
+    break;
+  case "u":
+  case "undo":
+    console.log("undo mode");
+    break;
+  case "l":
+  case "list":
+    console.log("undo mode");
+    break;
 
-console.log(args);
-
-console.log(
-  chalk.blue.bgRed.bold(await ask("i'll echo anything but in blue lol"))
-);
+  default:
+    break;
+}
