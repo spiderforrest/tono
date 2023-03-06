b2d08e10ae325f0b02dba5b12509235515ca828d
spiderforrest <spider@spood.org>
2023-03-05 20:39:57 -0800

    reading config file set up

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 import chalk from "chalk";
 import { getopt } from "stdio";
+import { readFile } from "fs";
+const configFile = "./config.json";
+
+readFile(configFile, (err, data) => {
+  if (err) {
+    console.log(chalk.bgRed(err));
+    console.log(chalk.red("Could not load config"));
+    throw err;
+  }
+  console.log(JSON.parse(data));
+});
 
 const opt = getopt({
   _meta_: { minargs: 2 },
