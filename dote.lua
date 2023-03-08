1d36e92113dad4a0bd25d5bcee02f5fa85b6ca13
spiderforrest <spider@spood.org>
2023-03-07 20:38:28 -0800

    added loicense and luarocks

--- dote.lua
+++ dote.lua
@@ -1,5 +1,23 @@
 #!/usr/bin/env lua
 
+ -- Copyright (C) 2023 Spider Forrest
+ -- contact: dote@spood.org
+ --
+ -- This program is free software: you can redistribute it and/or modify
+ -- it under the terms of the GNU Affero General Public License as published
+ -- by the Free Software Foundation, either version 3 of the License, or
+ -- (at your option) any later version.
+ --
+ -- This program is distributed in the hope that it will be useful,
+ -- but WITHOUT ANY WARRANTY; without even the implied warranty of
+ -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ -- GNU Affero General Public License for more details.
+ --
+ -- You should have received a copy of the GNU Affero General Public License
+ -- along with this program, at /LICENSE. If not, see <https://www.gnu.org/licenses/>.
+
 io.write("basic echo, will say whatever back in red\n")
 local input = io.read("*line")
 io.write('\27[31m' .. input ..'\n')
+
+
