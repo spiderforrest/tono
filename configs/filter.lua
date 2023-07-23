6db4038badbf09b5a834fa27446cb246009bdd0d
spiderforrest <spider@spood.org>
2023-07-23 12:43:55 -0700

    added a couple more filters

--- configs/filter.lua
+++ configs/filter.lua
@@ -7,6 +7,8 @@ local M = {}
 -- inside the item table passed into the function. If you create a
 -- custom field, it'll be named as you named it and just as accessible.
 
+-- this one is special and auto called w/o arguments but every other
+-- filter is just called by name with the text of the arg
 M.default = function (item)
     if item.type ~= "tag" then return true end
     return false
@@ -14,6 +16,21 @@ end
 
 M.all = function (_)
     return true
+end
+
+M.tags = function (item)
+    if item.type == "tag" then return true end
+    return false
+end
+
+M.todos = function (item)
+    if item.type == "todo" then return true end
+    return false
+end
+
+M.notes = function (item)
+    if item.type == "note" then return true end
+    return false
 end
 
 return M
